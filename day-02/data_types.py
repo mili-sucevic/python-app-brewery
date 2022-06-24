75e01990d5b5f0410a0c425764d866801e51964a
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-06-24 08:50:54 +0100

    Update data_types.py
    update first_digit and second_digit variables to int

--- day-02/data_types.py
+++ day-02/data_types.py
@@ -4,9 +4,10 @@
 two_digit_number = input("Type a two digit number: ")
 # print(type(two_digit_number))
 
-first_digit = two_digit_number[0]
-second_digit = two_digit_number[1]
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+# print(type(first_digit))
 
-result = int(first_digit) + int(second_digit)
+result = (first_digit) + (second_digit)
 # print(type(result))
 print(result)
