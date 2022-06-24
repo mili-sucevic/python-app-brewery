a4af32f9ddb338dce0311c4df6abe143f46d8b92
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-06-24 18:55:37 +0100

    Update 3_life_in_weeks.py
    add solution 2

--- day-02/3_life_in_weeks.py
+++ day-02/3_life_in_weeks.py
@@ -29,3 +29,13 @@ result_weeks = int(result) * int(weeks)
 result_months = int(result) * int(months)
 
 print(f"You have {result_days} days, {result_weeks} weeks, and {result_months} months left.")
+
+# SOLUTION 2
+age_as_int = int(age)
+
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
