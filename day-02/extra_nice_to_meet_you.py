b1db00c5510b9e173ca68bf44205106f8c315e73
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-06-24 09:06:57 +0100

    Create extra_nice_to_meet_you.py

---
+++ day-02/extra_nice_to_meet_you.py
@@ -0,0 +1,9 @@
+print('What is your name? ')
+myName = input()
+print("\nIt is good to meet you " + myName)
+a = len(myName)
+print("The length of your name is: " + str(a))
+
+print("\nHow old are you?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
