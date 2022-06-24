e3186e1de6ffb4b8ade939ddad8bc0d452d1953d
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-06-24 08:48:24 +0100

    Update and rename data_type.py to data_types.py

--- day-02/data_types.py
+++ day-02/data_types.py
@@ -1,5 +1,5 @@
 # Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
-#Warning. Do not change the code on lines 4-5. Your program should work for different inputs. e.g. any two-digit number.
+# Warning. Do not change the code on lines 4-5. Your program should work for different inputs. e.g. any two-digit number.
 
 two_digit_number = input("Type a two digit number: ")
 # print(type(two_digit_number))
