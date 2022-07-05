365460fe3a869619ce9419d031bad5053fc43938
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-07-05 08:04:17 +0100

    Update day-2-project-tip-calculator.py
    add final_amount, round and add print function in the end with f string

--- day-02/day-2-project-tip-calculator.py
+++ day-02/day-2-project-tip-calculator.py
@@ -21,5 +21,6 @@ people = int(input("How many people to split the bill? "))
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
-split_bill = total_bill / people
-print(total_bill)
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay ${final_amount}")
