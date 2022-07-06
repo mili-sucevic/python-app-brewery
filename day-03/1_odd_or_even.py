4d0a155208c568de9bf2652a525bd0760158ebd5
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-07-06 08:45:56 +0100

    Update 1_odd_or_even.py
    add solution 2

--- day-03/1_odd_or_even.py
+++ day-03/1_odd_or_even.py
@@ -36,7 +36,15 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
-if number % 2 == 0:
+# Solution 1
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# Solution 2
+remainder = number % 2
+if remainder == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
