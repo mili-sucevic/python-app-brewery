738f9eeffb855f2a1610dfec9a9b958ed2921ea0
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2022-07-07 18:18:42 +0100

    Update day-03-project-treasure-island.py
    add new lines in input functions

--- day-03/day-03-project-treasure-island.py
+++ day-03/day-03-project-treasure-island.py
@@ -27,21 +27,21 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-choice1 = input("\nYou're at a crossroad. Where do you want to go? Type 'left' or 'right': ").lower()
+choice1 = input("\nYou're at a crossroad. Where do you want to go? Type 'left' or 'right':\n").lower()
 
 if choice1 == "left":
 # Continue in the game
-  choice2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across: ").lower()
+  choice2 = input("\nYou've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across:\n").lower()
   if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?: ").lower()
+    choice3 = input("\nYou arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?:\n").lower()
     if choice3 == "red":
-      print("It's a room full of fire. Game Over.")
+      print("\nIt's a room full of fire. Game Over.")
     elif choice3 == "yellow":
-      print("You found the treasure! You Win!")
+      print("\nYou found the treasure! You Win!")
     elif choice3 == "blue":
-      print("You enter a room of beasts. Game Over.")
+      print("\nYou enter a room of beasts. Game Over.")
     else:
-      print("You chose a door that doesn't exist. Game Over.")
+      print("\nYou chose a door that doesn't exist. Game Over.")
 # Game will continue 
   else:
     print("You got attacked by an angry trout. Game Over")
